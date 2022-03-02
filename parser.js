c415455628f7d9daa00b0839b734d3f8b65d6165
vadzim.halakou <temakerazzy132@gmail.com>
2022-03-02 10:07:49 +0300

    set headers

--- parser.js
+++ parser.js
@@ -8,6 +8,9 @@ router.post('/', async (req, res) => {
         let {file} = req.files
         const workbook = XLSX.read(file.data);
         return res.json({data: workbook.Sheets})
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "*");
+        res.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept");
     } catch (e) {
         console.log(e, 'error')
     }
@@ -15,6 +18,9 @@ router.post('/', async (req, res) => {
 
 router.get('/', async (req, res) => {
     try {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "*");
+        res.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept");
         return res.json({asd:321})
     } catch (e) {
         console.log(e, 'error')
